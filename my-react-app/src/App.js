60b666125887a4b2c8ffcd9a1885365bad2b4d36
Razib Hasan <razib.hasan@dnet.org.bd>
2024-05-07 00:17:12 +0600

    firstcommit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let msg="Welcome to my React Page";
+  return (
+    <div className="App">
+      <h1>{msg}</h1>
+    </div>
+  );
+}
+
+export default App;
